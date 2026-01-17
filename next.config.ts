93fb9dcd4449d9fff959c4bf982497fa44763b5a
Necati Mazmanoğlu <necatimazmanoglu@gmail.com>
2026-01-18 01:51:31 +0300

    Next config guncellendi iyzipay build hatasi cozuldu

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Iyzipay hatasını çözen sihirli satır:
+  serverExternalPackages: ["iyzipay"],
+
   images: {
     remotePatterns: [
       {
